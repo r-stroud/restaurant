26f6d4246340c46c2e9843436171c57dac87f57b
r-stroud <stroudrobert86@gmail.com>
2022-08-07 21:35:35 -0500

    added images to nav bar, small css additions, scripts page for myself, and h1 on index

---
+++ scripts/robert.js
@@ -0,0 +1,16 @@
+const party = {
+    partyID: 1,
+    partySize: 4,
+    tableID: 3,
+    order: {
+        menuID: 1,
+        menuID: 4,
+        menuID: 6,
+        menuID: 5,
+        drinkID: 3,
+        drinkID: 1,
+        drinkID: 5,
+        drinkID: 5
+    }
+
+}
