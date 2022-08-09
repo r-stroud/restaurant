da38ff23b10cc6efabb49abac52bef333413511b
r-stroud <stroudrobert86@gmail.com>
2022-08-08 21:13:17 -0500

    added changes to index css and scripts

--- scripts/robert.js
+++ scripts/robert.js
@@ -1,32 +1,64 @@
-const party = {
-    partyID: 1,
-    partySize: 4,
-    tableID: 3,
-    order: [
-        {
-            menuID: 1,
-            drinkID: 3,
-            partyrestrictionID: 1,
-        },
-        {
-            menuID: 4,
-            drinkID: 1,
-            partyrestrictionID: 3,
-        },
-        {
-            menuID: 6,
-            drinkID: 5,
-            partyrestrictionID: 1,
-        },
-        {
-            menuID: 5,
-            drinkID: 5,
-            partyrestrictionID: 1,
-        }
-    ]
-}
+const partyRS = [
+    {
+        partyID: 1,
+        partySize: 4,
+        tableID: 3,
+        order: [
+            {
+                menuID: 1,
+                drinkID: 3,
+                partyrestrictionID: 1,
+            },
+            {
+                menuID: 4,
+                drinkID: 1,
+                partyrestrictionID: 3,
+            },
+            {
+                menuID: 6,
+                drinkID: 5,
+                partyrestrictionID: 1,
+            },
+            {
+                menuID: 5,
+                drinkID: 5,
+                partyrestrictionID: 1,
+            }
+        ]
+    },
+    {
+        partyID: 2,
+        partySize: 2,
+        tableID: 1,
+        order: [
+            {
+                menuID: 1,
+                drinkID: 3,
+                partyrestrictionID: 1,
+            },
+            {
+                menuID: 4,
+                drinkID: 1,
+                partyrestrictionID: 3,
+            },
+            {
+                menuID: 6,
+                drinkID: 5,
+                partyrestrictionID: 1,
+            },
+            {
+                menuID: 5,
+                drinkID: 5,
+                partyrestrictionID: 1,
+            }
+        ]
+    }
+]
 
-const menu = [
+
+// document.getElementById('taylorText').innerHTML = taylor();
+
+const menuRS = [
     {
         menuID: 1,
         item: "Everything Pizza",
@@ -59,7 +91,7 @@ const menu = [
     }
 ]
 
-drinkMenu = [
+drinkMenuRS = [
     {
         drinkID: 1,
         item: "Water",
@@ -87,8 +119,33 @@ drinkMenu = [
     },
 ]
 
-const partyRestriction = [
+const partyRestrictionRS = [
     { Id: 1, restriction: 'none' },
     { Id: 2, restriction: 'gluten-free' },
     { Id: 3, restriction: 'peanut-free' },
 ];
+
+let test = ""
+
+let numRS = 1
+
+for (const parties of partyRS) {
+    test = test + `<h4>Party ID: ${parties.partyID}</h4>
+    <h4>Party Size: ${parties.partySize}</h4>
+    <h4>Table Number: ${parties.tableID}</h4>`
+    for (const orders of parties.order) {
+        test = test + `<h4>Order ${numRS++}: ${orders.menuID}, ${orders.drinkID}</h4>`
+    }
+
+}
+
+
+
+document.getElementById("test1").innerHTML = test
+
+for (menu of menuRS) {
+    for (const parties of PartyRS) {
+    }
+}
+
+if (menu.menuID)
